51d2ab7a257c5977268c9107ad26772304deed11
ChristianRV29 <kmylorv@gmail.com>
2022-05-31 19:10:47 -0500

    Homescreen added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '~src/navigation/StackNavigator';
-import { Image } from 'react-native';
+import { Image, Text } from 'react-native';
 import { globalStyles } from '~src/theme/styles';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type HomeScreenProps = NativeStackNavigationProp<
   RootStackParamList,
@@ -11,12 +12,14 @@ type HomeScreenProps = NativeStackNavigationProp<
 >;
 
 const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
   return (
     <>
       <Image
         source={require('~src/assets/pokebola.png')}
         style={globalStyles.pokeballBG}
       />
+      <Text style={{ ...globalStyles.title, top: top + 20 }}>Pokedex</Text>
     </>
   );
 };
--- src/theme/styles/index.tsx
+++ src/theme/styles/index.tsx
@@ -12,4 +12,8 @@ export const globalStyles = StyleSheet.create({
     right: -100,
     opacity: 0.2,
   },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
 });
