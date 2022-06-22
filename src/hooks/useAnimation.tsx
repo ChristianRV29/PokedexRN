4ec604d800d50141abf84d36fb2dc86f41781094
ChristianRV29 <kmylorv@gmail.com>
2022-06-22 16:44:02 -0500

    useAnimation's function created

--- src/hooks/useAnimation.tsx
+++ src/hooks/useAnimation.tsx
@@ -13,5 +13,25 @@ export const useAnimation = () => {
     }).start();
   };
 
-  const fadeOut = () => {};
+  const fadeOut = (duration: number = 300) => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const startMovingPosition = (
+    initPosition: number,
+    duration: number = 300,
+  ) => {
+    position.setValue(initPosition);
+    Animated.timing(position, {
+      toValue: 0,
+      duration,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return { fadeIn, fadeOut, startMovingPosition };
 };
