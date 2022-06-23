8f11dfb6d943d754c2e933ab671b56d78a23ca4d
ChristianRV29 <kmylorv@gmail.com>
2022-06-23 17:20:59 -0500

    PokemonCard updated

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+} from 'react-native';
 
 import { SimplePokemon } from '~src/@types/interfaces/pokemon';
 
@@ -8,9 +14,37 @@ interface Props {
 }
 
 export const PokemonCard: React.FC<Props> = ({ pokemon }) => {
+  const { width: windowWidth } = Dimensions.get('window');
+
+  const { name, id } = pokemon;
+
   return (
-    <View>
-      <Text>{pokemon.name}</Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.8}>
+      <View style={{ ...styles.cardContainer, width: windowWidth * 0.4 }}>
+        <View>
+          <Text style={styles.name}>
+            {name}
+            {'\n#' + id}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    backgroundColor: 'red',
+    borderRadius: 10,
+    height: 120,
+    marginBottom: 25,
+    marginHorizontal: 20,
+  },
+  name: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    top: 20,
+    left: 10,
+  },
+});
