f26d3c016dcf00a4cbd8776d973ffe9fb3a4c5e4
ChristianRV29 <kmylorv@gmail.com>
2022-06-29 09:30:15 -0500

    Add movements to PokemonScreen

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, ScrollView, StyleSheet, Text } from 'react-native';
 
 import { PokemonDetails as Pokemon } from '~src/@types/interfaces/pokemon';
+import { FadeInImage } from './FadeInImage';
 
 interface Props {
   pokemon: Pokemon;
@@ -11,12 +12,54 @@ export const PokemonDetails: React.FC<Props> = props => {
   const { pokemon } = props;
 
   return (
-    <ScrollView style={{ ...StyleSheet.absoluteFillObject }}>
+    <ScrollView
+      style={{ ...StyleSheet.absoluteFillObject }}
+      showsVerticalScrollIndicator={false}>
       <View style={styles.container}>
         <Text style={styles.title}>Type</Text>
         <View style={styles.typesWrapper}>
           {pokemon.types.map(({ type }) => (
-            <Text key={type.name}>{type.name}</Text>
+            <Text style={styles.typeText} key={type.name}>
+              {type.name}
+            </Text>
+          ))}
+        </View>
+        <Text style={styles.title}>Weight</Text>
+        <Text>{pokemon.weight}Kg</Text>
+        <Text style={styles.title}>Sprites</Text>
+        <ScrollView horizontal>
+          <FadeInImage
+            uri={pokemon.sprites.front_default}
+            style={styles.sprite}
+          />
+          <FadeInImage
+            uri={pokemon.sprites.back_default}
+            style={styles.sprite}
+          />
+          <FadeInImage
+            uri={pokemon.sprites.front_shiny}
+            style={styles.sprite}
+          />
+          <FadeInImage uri={pokemon.sprites.back_shiny} style={styles.sprite} />
+        </ScrollView>
+        <Text style={styles.title}>Basic abilities</Text>
+        <View style={styles.typesWrapper}>
+          {pokemon.abilities.map(({ ability }) => (
+            <Text style={styles.typeText} key={ability.name}>
+              {ability.name}
+            </Text>
+          ))}
+        </View>
+        <Text style={styles.title}>Movements</Text>
+        <View
+          style={{
+            ...styles.typesWrapper,
+            flexWrap: 'wrap',
+          }}>
+          {pokemon.moves.map(({ move }) => (
+            <Text style={styles.typeText} key={move.name}>
+              {move.name}
+            </Text>
           ))}
         </View>
       </View>
@@ -31,6 +74,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 30,
     fontWeight: 'bold',
+    marginTop: 20,
   },
   typesWrapper: {
     flexDirection: 'row',
@@ -38,5 +82,9 @@ const styles = StyleSheet.create({
   typeText: {
     marginRight: 10,
     fontSize: 17,
+  },
+  sprite: {
+    width: 100,
+    height: 100,
   },
 });
