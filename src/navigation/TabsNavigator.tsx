fccbb4c3b17f019661a32295335f7a129dd155ca
ChristianRV29 <kmylorv@gmail.com>
2022-06-30 08:38:04 -0500

    Implement BottomTabNavigator

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -3,14 +3,46 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import StackNavigator from '~src/navigation/StackNavigator';
 import SearchScreen from '~src/screens/SearchScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const TabsNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="StackNavigator" component={StackNavigator} />
-      <Tab.Screen name="SearchScreen" component={SearchScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#5856D5',
+        tabBarStyle: {
+          backgroundColor: 'rgba (255, 255, 255, 0.92)',
+          borderWidth: 0,
+          elevation: 0,
+          height: 60,
+          paddingBottom: 10,
+          position: 'absolute',
+        },
+      }}
+      sceneContainerStyle={{ backgroundColor: 'white' }}>
+      <Tab.Screen
+        name="StackNavigator"
+        component={StackNavigator}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <Icon color={color} size={20} name="home-outline" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SearchScreen"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color }) => (
+            <Icon color={color} size={20} name="search-outline" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
