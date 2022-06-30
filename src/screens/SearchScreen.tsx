9d1b79366cd6b96d4edd2ca08011918f6a8796ae
ChristianRV29 <kmylorv@gmail.com>
2022-06-30 08:06:27 -0500

    Install BottomTabNavigator dependecy

---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
