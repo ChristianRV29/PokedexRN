1df174dbdf7f17c2956afc304513adba7043f6df
ChristianRV29 <kmylorv@gmail.com>
2022-07-01 11:26:50 -0500

    Add SearchInput component

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Platform, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SearchInput } from '~src/components/SearchInput';
 
 const SearchScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? top : top + 10,
+        marginHorizontal: 20,
+      }}>
+      <SearchInput />
     </View>
   );
 };
