b4c87e5aff97df2389a9b09f097f2a8031281801
ChristianRV29 <kmylorv@gmail.com>
2022-07-11 07:48:49 -0500

    Handle loading state in SearchScreen

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,22 +1,51 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { Platform, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Platform,
+  View,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { SearchInput } from '~src/components/SearchInput';
+import { usePokemonSearch } from '~src/hooks/usePokemonSearch';
 
 const SearchScreen = () => {
   const { top } = useSafeAreaInsets();
 
+  const { isFetching } = usePokemonSearch();
+
+  if (isFetching) {
+    return (
+      <View style={styles.activityWrapper}>
+        <ActivityIndicator size={50} color="grey" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
-        flex: 1,
+        ...styles.searchWrapper,
         marginTop: Platform.OS === 'ios' ? top : top + 10,
-        marginHorizontal: 20,
       }}>
       <SearchInput />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  activityWrapper: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  searchWrapper: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+});
 
 export default SearchScreen;
